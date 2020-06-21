4ac3c979aaa08eae5c3d1d1b603e3af4cc111bb4
kamioka24 <keee2404@gmail.com>
2020-06-21 21:48:32 +0900

    [Add] team_devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,30 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	def after_sign_in_path_for(resource)
+        case resource
+        when :player
+        when :team
+        end
+    end
+
+    def after_sign_out_path_for(resource)
+        case resource
+        when :player
+        when :team
+        end
+    end
+
+    def after_sign_up_path_for(resource)
+        case resource
+        when :player
+        when :team
+        end
+    end
 
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :prefecture_code, :email, :encrypted_password])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :member, :team_image, :introduction,
+			:prefecture_code, :city, :street, :phone_number, :category, :email, :encrypted_password])
 	end
 end
