e8fd4067de8256453401b620d78509a379228a80
kamioka24 <keee2404@gmail.com>
2020-06-22 11:05:14 +0900

    [Add] player_controller

---
+++ app/controllers/player/players_controller.rb
@@ -0,0 +1,21 @@
+class PlayersController < ApplicationController
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def exit
+	end
+
+	def destroy
+	end
+
+	private
+
+	def player_paramas
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/about'
+  get 'bookmarks/index'
+  get 'posts/index'
+  get 'posts/show'
+  get 'teams/index'
+  get 'teams/show'
+  get 'players/show'
+  get 'players/edit'
+  get 'players/exit'
 	devise_for :players
 	devise_for :teams
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
