5bf1dd922aeeadcfe797c1c170666fb616030f40
kamioka24 <keee2404@gmail.com>
2020-06-24 23:59:30 +0900

    [Update] footer_position

--- app/controllers/player/posts_controller.rb
+++ app/controllers/player/posts_controller.rb
@@ -1,7 +1,10 @@
 class Player::PostsController < ApplicationController
 	def index
+		@post = Post.find(params[:id])
+		@posts = Post.page(params[:page]).per(10).order(created_at: "DESC") #降順
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 end
