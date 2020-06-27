13d6bd3234ba187712d0e5be95b485ac5c7d9aec
kamioka24 <keee2404@gmail.com>
2020-06-27 10:51:26 +0900

    [Update_view] player_team_show

--- app/controllers/player/bookmarks_controller.rb
+++ app/controllers/player/bookmarks_controller.rb
@@ -1,12 +1,19 @@
 class Player::BookmarksController < ApplicationController
 	def index
+		@teams = current_player.bookmark_teams.find(params[:team_id])
 	end
 
 	def create
 		@team = Team.find(params[:team_id])
+		bookmark = current_player.bookmarks.new(team_id: params[:team_id])
+		bookmark.save
+		redirect_back(fallback_location: root_path) # bookmarkする前の画面に戻る
 	end
 
 	def destroy
 		@team = Team.find(params[:team_id])
+		bookmark = current_player.bookmarks.find_by(team_id: @team.id)
+		bookmark.destroy
+		redirect_back(fallback_location: root_path) # bookmarkする前の画面に戻る
 	end
 end
