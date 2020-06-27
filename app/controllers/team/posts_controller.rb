f775d9e820d4a3b9c8f7a400b94821d441a583d6
kamioka24 <keee2404@gmail.com>
2020-06-27 16:48:27 +0900

    [Add] seach_box

--- app/controllers/team/posts_controller.rb
+++ app/controllers/team/posts_controller.rb
@@ -15,6 +15,8 @@ class Team::PostsController < ApplicationController
 
 	def index
 		@posts = Post.page(params[:page]).per(10).order(created_at: "DESC") #降順
+		@q = Post.ransack(params[:q])
+		@post = @q.result(distinct: true)
 	end
 
 	def show
