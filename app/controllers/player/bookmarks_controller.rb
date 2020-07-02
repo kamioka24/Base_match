951a6bacdf012f9e64fd53266273255a10b52c19
kamioka24 <keee2404@gmail.com>
2020-07-02 16:55:27 +0900

    [Add_Ajax] bookmark

--- app/controllers/player/bookmarks_controller.rb
+++ app/controllers/player/bookmarks_controller.rb
@@ -7,15 +7,13 @@ class Player::BookmarksController < ApplicationController
 
 	def create
 		@team = Team.find(params[:team_id])
-		bookmark = current_player.bookmarks.new(team_id: params[:team_id])
-		bookmark.save
-		redirect_back(fallback_location: root_path) # bookmarkする前の画面に戻る
+		@bookmark = current_player.bookmarks.new(team_id: @team.id)
+		@bookmark.save
 	end
 
 	def destroy
 		@team = Team.find(params[:team_id])
-		bookmark = current_player.bookmarks.find_by(team_id: @team.id)
-		bookmark.destroy
-		redirect_back(fallback_location: root_path) # bookmarkする前の画面に戻る
+		@bookmark = current_player.bookmarks.find_by(team_id: @team.id)
+		@bookmark.destroy
 	end
 end
