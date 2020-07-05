2ea17fcef48cda593df0c33e1fffd2c6968f636f
kamioka24 <keee2404@gmail.com>
2020-07-05 23:09:47 +0900

    [Update] contact_mail.html.erb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,18 +1,19 @@
 class ContactMailer < ApplicationMailer
-
-	# Subject can be set in your I18n file at config/locales/en.yml
-	# with the following lookup:
-	#
-	#   en.contact_mailer.contact_mail.subject
-	#
-	def contact_mail
-		@greeting = "Hi"
-
-		mail to: "to@example.org"
-	end
+	default from: ENV["DEV_SMTP_USER_NAME"]
 
 	def contact_mail(contact)
 	    @contact = contact
-	    mail to: @team.email, subject: "メールのタイトル"
+	    mail to: @team.email, subject: "選手からのお問い合わせが届きました。"
 	end
+
+	# # Subject can be set in your I18n file at config/locales/en.yml
+	# # with the following lookup:
+	# #
+	# #   en.contact_mailer.contact_mail.subject
+	# #
+	# def contact_mail
+	# 	@greeting = "Hi"
+
+	# 	mail to: "to@example.org"
+	# end
 end
