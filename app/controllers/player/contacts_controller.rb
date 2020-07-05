e51e1a5740b5de324bd38bab17b3e3a4dd7752eb
kamioka24 <keee2404@gmail.com>
2020-07-05 20:05:55 +0900

    [Add controller] contact & [Add view] contact_new/complete

---
+++ app/controllers/player/contacts_controller.rb
@@ -0,0 +1,27 @@
+class Player::ContactsController < ApplicationController
+	before_action :authenticate_player!
+	def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+		if @contact.save
+		   ContactMailer.contact_mail(@contact).deliver
+		   redirect_to complete_player_contacts_path
+		else
+		   render "new"
+		end
+	end
+
+	def complete # 送信完了画面
+		@team = Team.find(params[id])
+	end
+
+	private
+
+	def contact_params
+		params.require(:contact).permit(:player_id, :name, :school, :year, :address,
+		                                :phone_number, :choice, :day, :question)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
 	devise_for :players, :controllers => {
 		:sessions => 'player/sessions',
 		:registrations => 'player/registrations'
