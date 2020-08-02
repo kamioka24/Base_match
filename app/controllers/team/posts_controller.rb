9a4191ba2477d3bc24bb9b1cc278938490435a5c
keisuke kamioka <keee2404@gmail.com>
2020-08-02 00:53:47 +0000

    [Update] post_controller

--- app/controllers/team/posts_controller.rb
+++ app/controllers/team/posts_controller.rb
@@ -19,7 +19,7 @@ class Team::PostsController < ApplicationController
 	def index
 		@q = Post.ransack(params[:q])
 		if params[:team].nil? # 投稿一覧出力時に、チームidが含まれているかどうか。
-			@posts = @q.result(distinct: true).page(params[:page]).per(15).order(created_at: "DESC")
+			@posts = @q.result(distinct: true).page(params[:page]).per(12).order(created_at: "DESC")
 		else
 			@posts = @q.result(distinct: true).where(team_id: params[:team]).page(params[:page]).per(10).order(created_at: "DESC")
 		end
